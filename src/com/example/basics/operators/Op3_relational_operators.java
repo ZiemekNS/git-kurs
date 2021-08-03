c39fde6fff6109e5e220b878f95da41643ebb965
ZiemekNS <ziemek-1994@o2.pl>
2021-08-03 22:03:32 +0200

    Relational operators

---
+++ src/com/example/basics/operators/Op3_relational_operators.java
@@ -0,0 +1,25 @@
+package com.example.basics.operators;
+
+public class Op3_relational_operators {
+
+    public static void main(String args[]) {
+        boolean b1 = 5 == 3;
+        System.out.println(b1);
+
+        boolean b2 = 7 == 7;
+        System.out.println(b2);
+
+        boolean b3 = 7 != 7;
+        System.out.println(b3);
+
+        boolean b4 = 7 != 5;
+        boolean b5 = 10 > 4;
+        boolean b6 = 10 < 8;
+
+        boolean b7 = 8 >= 6;
+        boolean b8 = 8 >= 8;
+        boolean b9 = 8 > 8;
+        boolean b10 = 10 <= 12;
+        boolean b11 = 10 <= 10;
+    }
+}
