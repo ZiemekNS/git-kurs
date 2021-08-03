4a0dde5cbf415a3aa4d502ffecb8be33a1c41a54
ZiemekNS <ziemek-1994@o2.pl>
2021-08-03 21:53:44 +0200

    Added incrementation and decrementation class

---
+++ src/com/example/basics/operators/Op2_incrementation_decrementation.java
@@ -0,0 +1,20 @@
+package com.example.basics.operators;
+
+public class Op2_incrementation_decrementation {
+
+    public static void main(String args[]) {
+        int a = 10;
+        a++;
+        System.out.println("a++:" +a);
+
+        int b = 5;
+        int c = 10 + b++;
+        System.out.println("c++: "+c);
+        System.out.println("b++: "+b);
+
+        int d = 5;
+        int e = 10 + ++d;
+        System.out.println("d: "+d);
+        System.out.println("e: "+e);
+    }
+}
