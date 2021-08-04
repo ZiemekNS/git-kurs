49dc2048a9822da2a43ec41cb26a2d3b63e2ca92
ZiemekNS <ziemek-1994@o2.pl>
2021-08-04 05:20:56 +0200

    added logical operator and class

---
+++ src/com/example/basics/operators/Op4_logicalOperatorAnd.java
@@ -0,0 +1,12 @@
+package com.example.basics.operators;
+
+public class Op4_logicalOperatorAnd {
+
+    public static void main(String args[]) {
+        // koniunkcja - Operator logiczny, inaczej and
+        System.out.println(5 < 10 && 20 >=15); // true && true, wynik true
+        System.out.println(5 > 10 && 20 > 15); // false && true, wynik false
+        System.out.println(4 > 2 && 10 == 11); // true && false, wynik false
+        System.out.println( 4 == 2 && 12 > 20); // false && false, wynik false
+    }
+}
