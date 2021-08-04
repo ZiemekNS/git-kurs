b7a2fe038eebedd41035d41304416525f22f27d6
ZiemekNS <ziemek-1994@o2.pl>
2021-08-04 05:25:34 +0200

    added logical operators or class

---
+++ src/com/example/basics/operators/Op5_logicalOperatorOr.java
@@ -0,0 +1,11 @@
+package com.example.basics.operators;
+
+public class Op5_logicalOperatorOr {
+
+    public static void main(String args[]) {
+        System.out.println(10 > 2 || 5 >= 1); //true
+        System.out.println(10 > 8 || 1 == 10); //true
+        System.out.println(3 < 8 || 1 == 1); //true
+        System.out.println(4 > 5 || 2 <= 1); //false    
+    }
+}
