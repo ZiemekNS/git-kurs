da3a7ef775d7d56b613d216213cadd6f4d833b3f
ZiemekNS <ziemek-1994@o2.pl>
2021-08-05 05:39:29 +0200

    Added ifStatement class

---
+++ src/com/example/basics/control_flow_statements/IfStatement.java
@@ -0,0 +1,22 @@
+package com.example.basics.control_flow_statements;
+
+public class IfStatement {
+
+    public static void main(String args[]) {
+
+        int a =3;
+        if (a  > 5) {
+            System.out.println("a jest wieksze od 5");
+        }
+        else if (a == 2) {
+            System.out.println("a jest rowne 2");
+        }
+        else if (a == 3)
+        {
+            System.out.println(("a jest rowne 3"));
+        }
+        else {
+            System.out.println("wartość a jest równa: " + a);
+            }
+    }
+}
