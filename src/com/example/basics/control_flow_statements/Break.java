6bd23e4951743f4f605ff5456e3a0552d1c1e44a
ZiemekNS <ziemek-1994@o2.pl>
2021-08-11 06:01:22 +0200

    Added Break, Continue, ForEach, InfiniteLoop and started BasicScanner class

---
+++ src/com/example/basics/control_flow_statements/Break.java
@@ -0,0 +1,25 @@
+package com.example.basics.control_flow_statements;
+
+public class Break {
+
+    public static void main(String args[]) {
+
+        for (int i = 0; i < 20; i++){
+            System.out.println(i);
+
+            if (i > 3) break;
+            }
+
+        System.out.println("Dalsza część programu");
+
+
+        int a = 10;
+        while(a > 0) {
+            System.out.println(a);
+            a--;
+
+            if (a == 2) break;
+        }
+            System.out.println("Petla while zakończona");
+        }
+    }
---
+++ src/com/example/basics/programs/BasicScanner.java
@@ -0,0 +1,15 @@
+package com.example.basics.programs;
+
+import java.util.*;
+
+public class BasicScanner {
+
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Podaj swoje imię");
+        String main = in.nextLine();
+        System.out.println("Twoje imię to: " +in);
+    }
+}
