204857245849af6cb4f412983519a7576d82a96f
ZiemekNS <ziemek-1994@o2.pl>
2021-08-19 23:05:35 +0200

    Added overloading class and invoking method class

--- src/com/example/basics/methods/InvokingMethods.java
+++ src/com/example/basics/methods/InvokingMethods.java
@@ -1,9 +1,35 @@
 package com.example.basics.methods;
 
+class Triangle {
+    float a; //dlugosc podstawy trojkata
+    float h; // wysokosc trojkata
+    float surfaceArea; //pole powierzchni
+    Triangle(float base, float high){
+        a = base;
+        h = high;
+    }
+}
+
+class MyMath {
+
+    public void add(int a, int b){
+        a = 20;
+        int result = a + b;
+        System.out.println("Result: " + result);
+
+    }
+}
+
 public class InvokingMethods {
 
     public static void main(String[] args) {
 
-        
+    MyMath math = new MyMath();
+    int a = 10;
+    int b = 7;
+    math.add(a,b);
+
+    Triangle triangle = new Triangle(10, 5);
+
     }
 }
---
+++ src/com/example/basics/methods/ReturnValueAndObject.java
@@ -0,0 +1,39 @@
+package com.example.basics.methods;
+
+class Circle {
+    float radius;
+
+    Circle(float r) {
+        radius =r;
+    }
+}
+
+class MathMethods {
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int substract(int a, int b){
+        return a-b;
+    }
+
+    public Circle getNewCircle(float radius){
+        Circle c = new Circle(radius);
+        return c;
+    }
+}
+
+
+public class ReturnValueAndObject {
+    public static void main(String[] args) {
+        MathMethods math = new MathMethods();
+        int a = 12;
+        int b = 3;
+        int result = math.substract(a,b);
+        System.out.println("Result: "+result);
+
+        Circle circle = math.getNewCircle(10.5f);
+        System.out.println("Radius: " + circle.radius);
+    }
+
+}
