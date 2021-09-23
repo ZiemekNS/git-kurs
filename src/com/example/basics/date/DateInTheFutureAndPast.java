404c60e75f2764233bd7500054e50302c5c0e44d
ZiemekNS <ziemek-1994@o2.pl>
2021-09-23 23:31:44 +0200

    Learned about Calendar and Time method

---
+++ src/com/example/basics/date/DateInTheFutureAndPast.java
@@ -0,0 +1,39 @@
+package com.example.basics.date;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateInTheFutureAndPast {
+    public static void main(String[] args) {
+
+        Date dateNow = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateNow);
+        System.out.println(dateNow);
+
+        calendar.add(Calendar.DAY_OF_MONTH, 2);
+        calendar.add(Calendar.MONTH, 2);
+        calendar.add(Calendar.YEAR, 1);
+        calendar.add(Calendar.HOUR, 1);
+        calendar.add(Calendar.MINUTE, 10);
+        calendar.add(Calendar.SECOND, 30);
+
+        Date dateInTheFuture = calendar.getTime();
+        System.out.println(dateInTheFuture);
+
+        //date in the past
+
+        calendar.setTime(dateNow);
+
+        calendar.add(Calendar.DAY_OF_MONTH, -2);
+        calendar.add(Calendar.MONTH, -2);
+        calendar.add(Calendar.YEAR, -1);
+        calendar.add(Calendar.HOUR, -1);
+        calendar.add(Calendar.MINUTE, -10);
+        calendar.add(Calendar.SECOND, -30);
+
+        Date dateInThePast = calendar.getTime();
+        System.out.println(dateInThePast);
+
+    }
+}
